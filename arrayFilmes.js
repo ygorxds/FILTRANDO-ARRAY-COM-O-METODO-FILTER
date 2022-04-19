81de2be58096aaae1fa76b8b88b9aa6de9684024
ygorxds <ygordesa2010@hotmail.com>
2022-04-19 17:07:13 -0300

    frist commit

---
+++ arrayFilmes.js
@@ -0,0 +1,19 @@
+
+
+const filmes = [
+  { titulo: "Vingadores", categoria: "Ação" },
+  { titulo: "Duro de matar", categoria: "Ação" },
+  { titulo: "Gente Grande", categoria: "Comédia" },
+  { titulo: "Gente Grande 2", categoria: "Comédia" },
+  { titulo: "Iron Man", categoria: "Ação" },
+  { titulo: "How I met Your Mother", categoria: "Comédia" },
+  { titulo: "Winter on fire", categoria: "Documentário" },
+]
+
+
+function filterMovie(cat) {
+  return cat.categoria == t
+}
+
+console.log(filmes.filter(filterMovie, t = "Ação"))
+
